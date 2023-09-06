9a1fc17bd6f4b522bf4f04a33190d0d17020366f
Taylan Dogan <taylan.dogan@cognite.com>
2023-09-06 02:49:55 +0200

    fix: remove address
    Signed-off-by: Taylan Dogan <taylan.dogan@cognite.com>

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,4 +6,4 @@
 &kp C,             &kp N1,            &kp N2,                       &kp N3,            &kp T,             &kp Y,             &u_to_U_BASE,             &kp I,             &kp O,             &kp P,             \
 &kp X,             &kp Q,             &kp W,                        &kp E,             &kp R,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
 &kp Z,             &kp A,             &kp S,                        &kp D,             &kp F,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
-U_NP,              U_NP,              &kp U_MT(LSHFT, TAB),         &kp LCTRL,         &kp BSPC,          &kp TAB,          &kp SPACE,        &kp ESC,           U_NP,              U_NP
+U_NP,              U_NP,              U_MT(LSHFT, TAB),         &kp LCTRL,         &kp BSPC,          &kp TAB,          &kp SPACE,        &kp ESC,           U_NP,              U_NP
